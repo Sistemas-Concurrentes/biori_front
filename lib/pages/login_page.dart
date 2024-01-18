1a475719c085cc8e55f2f43a2e8e924f75184e13
JaviGP2002 <javigp2002@gmail.com>
2024-01-18 11:48:22 +0100

    :sparkles: Primera aproximación de login
    Login creado con dos campos y un boton de javiEditText que aún no conecta a nada ni realiza nada

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,211 @@
+import 'package:biori/pages/javiEditText.dart';
+import 'package:biori/pages/register_page.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
+import 'package:snippet_coder_utils/hex_color.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isApiCallProcess = false;
+  bool hidePassword = true;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  String? username;
+  String? password;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        body: ProgressHUD(
+          inAsyncCall: isApiCallProcess,
+          opacity: 0.3,
+          key: UniqueKey(),
+          child: Form(
+            key: globalFormKey,
+            child: _loginUI(context),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget usernameEditText() {
+    return JaviForms.inputBaseWidget(
+      context,
+      "username",
+      "Username",
+      (onValidateVal) {
+        if (onValidateVal.isEmpty) {
+          return 'Username can\'t be empty.';
+        }
+
+        return null;
+      },
+      (onSavedVal) => {username = onSavedVal},
+      prefixIcon: const Icon(Icons.person),
+    );
+  }
+
+  Widget passwordEditText() {
+    return JaviForms.inputBaseWidget(
+      context,
+      "password",
+      "Password",
+      (onValidateVal) {
+        if (onValidateVal.isEmpty) {
+          return 'password can\'t be empty.';
+        }
+
+        return null;
+      },
+      (onSavedVal) => {password = onSavedVal},
+      prefixIcon: const Icon(Icons.password),
+      suffixIcon: IconButton(
+        onPressed: () {
+          setState(() {
+            hidePassword = !hidePassword;
+          });
+        },
+        color: Theme.of(context).primaryColor.withOpacity(0.4),
+        icon: Icon(hidePassword ? Icons.visibility_off : Icons.visibility),
+      ),
+      obscureText: hidePassword,
+    );
+  }
+
+  Widget _loginUI(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 5,
+            decoration: BoxDecoration(
+                color: Theme.of(context).secondaryHeaderColor,
+                borderRadius: BorderRadius.circular(18.0),
+                boxShadow: const [
+                  BoxShadow(
+                      color: Colors.black12,
+                      offset: Offset(0, 2),
+                      blurRadius: 6.0)
+                ]),
+            child: const Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.center,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.0),
+                    child: Text(
+                      "Biori",
+                      style: TextStyle(
+                          fontSize: 28.0, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: usernameEditText()
+          ),
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child:passwordEditText()
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(
+                top: 10.0,
+                right: 20.0,
+              ),
+              child: RichText(
+                text: TextSpan(
+                  style: JaviStyle.granada1,
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "Forgot Password?",
+                      style: JaviStyle.url,
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const RegisterPage()),
+                          );
+                        },
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: FormHelper.submitButton(
+              "Login",
+              () {},
+              btnColor: Theme.of(context).primaryColor,
+              txtColor: Colors.white,
+              borderColor: Theme.of(context).primaryColor,
+            ),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          RichText(
+            text: TextSpan(
+              style: const TextStyle(
+                color: Colors.grey,
+                fontSize: 14.0,
+              ),
+              children: <TextSpan>[
+                const TextSpan(
+                  text: "¿No tienes una cuenta? ",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 14.0,
+                  ),
+                ),
+                TextSpan(
+                  text: "Registrate",
+                  style: JaviStyle.url,
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const RegisterPage()),
+                      );
+                    },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _myEditText {}
