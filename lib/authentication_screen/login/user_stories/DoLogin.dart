92533da59579801c667dd701c7a075b3b1c3e16d
JaviGP2002 <javigp2002@gmail.com>
2024-01-19 16:59:52 +0100

    :rocket: Add: logica de negocio de login.
    Con la nueva lógica de negocio se puede mejorar la independización de pantalla y lógica donde Añadimos un enum para la salida (mejorar a la hora que queramos cmabiar algo de dentor de la lógica)
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

---
+++ lib/authentication_screen/login/user_stories/DoLogin.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:biori/style/constants.dart';
+import 'package:http/http.dart' as http;
+
+enum Output {
+  success,
+  error,
+}
+
+class DoLogin {
+
+
+  Future<Output> run(username, password) async {
+    Output output = Output.error;
+
+    http.Response login = await loginUser(username, password);
+
+    if (login.statusCode == 200){
+      output = Output.success;
+    }
+
+    return output;
+
+  }
+
+
+  Future<http.Response> loginUser(String username, String password) {
+    return http.post(
+      Uri.parse(Constants.URI),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'username': username,
+        'password': password,
+      }),
+    );
+  }
+
+
+}
