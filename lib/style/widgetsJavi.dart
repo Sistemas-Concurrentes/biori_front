eb69d8b7a3d239f490bdcfc78ca497823a71480e
JaviGP2002 <javigp2002@gmail.com>
2024-01-21 14:10:29 +0100

    :zap: Creación de la los enlaces secundarios
    Ahora cuando queramos un texto y un texto con url para redirigir podemos hacerlo simplemente y se cambiará en todos los lugares donde lo utilicemos, la función para redirigir se pasará por parámetros
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/style/widgetsJavi.dart
+++ lib/style/widgetsJavi.dart
@@ -1,15 +1,24 @@
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 import 'javiEditText.dart';
 
 class WidgetsJavi {
-  Container bioriContainer(context){
+  Container bioriContainer(context) {
     return Container(
-      width: MediaQuery.of(context).size.width,
-      height: MediaQuery.of(context).size.height / 5,
+      width: MediaQuery
+          .of(context)
+          .size
+          .width,
+      height: MediaQuery
+          .of(context)
+          .size
+          .height / 5,
       decoration: BoxDecoration(
-          color: Theme.of(context).secondaryHeaderColor,
+          color: Theme
+              .of(context)
+              .secondaryHeaderColor,
           borderRadius: BorderRadius.circular(18.0),
           boxShadow: const [
             BoxShadow(
@@ -29,6 +38,28 @@ class WidgetsJavi {
                 style: JaviStyle.titulo,
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  RichText informacionSecundaria(BuildContext context, String textoSinUrl, String textoRedirigir, Function redirigir){
+    return RichText(
+      text: TextSpan(
+        style: JaviStyle.subcomentarios,
+        children: <TextSpan>[
+          TextSpan(
+            text: textoSinUrl,
+            style: JaviStyle.granada1,
+          ),
+          TextSpan(
+            text: textoRedirigir,
+            style: JaviStyle.url,
+            recognizer: TapGestureRecognizer()
+              ..onTap = () {
+                redirigir();
+              },
           ),
         ],
       ),
