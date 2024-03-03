2593b39cd394031c5034371114d23b7862371143
JaviGP2002 <javigp2002@gmail.com>
2024-03-03 13:23:22 +0100

    :construction: Do not work atm. Add Event card.
    Recibido un eventModel y una funcion callback para poder actualizar su información. Somos capaces de pintar el evento con los botones de las categorias que permiten interactuar con ellas, dar descripciones y poder presionar sobre la propia card para acceder al detalle de la misma en otra página. Manteniendo el callback para actualizar en ambos sitios el like.
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

---
+++ lib/main_screen/home/user_stories/events/widget/event_card.dart
@@ -0,0 +1,50 @@
+import 'package:biori/router/custom_router.dart';
+import 'package:biori/style/releases_widgets/button_widgets/categories_buttons.dart';
+import 'package:biori/style/releases_widgets/button_widgets/my_like_button.dart';
+import 'package:biori/style/releases_widgets/releases_widgets.dart';
+import 'package:flutter/material.dart';
+import '../model/event_model.dart';
+
+class EventCard extends StatelessWidget {
+  final scrollController = ScrollController();
+  final EventModel eventModel;
+  final Function(int) likeEvent;
+
+  EventCard(
+      {super.key, required this.likeEvent, required this.eventModel});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            ReleasesWidgets.releaseTitleRow(context, eventModel.title),
+            const CategoriesButtons(),
+            ReleasesWidgets.releaseDescriptionRow(
+                context, eventModel.description),
+            _ultimaLinea(context)
+          ],
+        ),
+      ),
+      onTap: () {
+        CustomRouter.router.push("/eventDetail/${eventModel.id}",
+            extra: {'eventModel': eventModel, 'likeEvent': likeEvent}).then((value) => null);
+      },
+    );
+  }
+
+  Row _ultimaLinea(context) {
+    return Row(
+      children: [
+        MyLikeButton(
+            idEvent: eventModel.id,
+            numberLikes:eventModel.numberLikes,
+            isLiked: eventModel.isLiked,
+            likeEvent: likeEvent),
+        ReleasesWidgets.moreInfoButton(context, eventModel.location,
+            eventModel.date, eventModel.endInscription),
+      ],
+    );
+  }
+}
