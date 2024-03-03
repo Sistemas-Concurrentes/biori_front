b85527a9c3fdc914305ff6363b456e42cd256dc1
JaviGP2002 <javigp2002@gmail.com>
2024-03-03 13:21:48 +0100

    :construction: Do not work atm. Add constants for UI
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

---
+++ lib/style/releases_widgets/constants/constants.dart
@@ -0,0 +1,9 @@
+class EventConstants {
+  static const double heightButtons = 30;
+  static const double margin = 10;
+  static const double heightTitle = 40;
+  static const double padding = 10;
+  static const double borderRadius = 16;
+  static const double maxWidth = 600;
+  static const double widthBorderSeparator = 1;
+}
