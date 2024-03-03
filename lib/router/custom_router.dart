92a582e630c000dc0e1519f241091067042f3386
JaviGP2002 <javigp2002@gmail.com>
2024-03-03 13:08:53 +0100

    :rocket: Do not works atm. Add NavBar and edit Router
    HEmos creado las "keys" para el router que mantienen el estado de la aplicación o el stack de las páginas. Cuando cambiamos por el navbar la página entre als esenciales, obtenemos una serie de branches donde se guardan los estados y posiciones de las páginas. Las KEYS van a ser muy recurrentes.
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/router/custom_router.dart
+++ lib/router/custom_router.dart
@@ -1,16 +1,30 @@
 import 'package:biori/authentication_screen/login/login_page.dart';
 import 'package:biori/authentication_screen/login/validation/validation_page.dart';
 import 'package:biori/authentication_screen/register/register_page.dart';
-import 'package:biori/keys/globals.dart';
-import 'package:biori/main_screen/main_screen.dart';
+import 'package:biori/main_screen/home/event_detail/event_detail_page.dart';
+import 'package:biori/main_screen/home/home_page.dart';
+import 'package:biori/main_screen/bottom_navigation_bar.dart';
+import 'package:biori/main_screen/settings/setting_page.dart';
 import 'package:biori/router/route_constants.dart';
 import 'package:biori/splash_screen/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../main_screen/home/user_stories/events/model/event_model.dart';
+
+
 class CustomRouter {
   static final CustomRouter _instance = CustomRouter._internal();
   static late final GoRouter router;
+
+  final GlobalKey<NavigatorState> parentNavigatorKey =
+      GlobalKey<NavigatorState>();
+  final GlobalKey<NavigatorState> homeTabNavigatorKey =
+      GlobalKey<NavigatorState>();
+  final GlobalKey<NavigatorState> profileTabNavigatorKey =
+      GlobalKey<NavigatorState>();
+  final GlobalKey<NavigatorState> settingsTabNavigatorKey =
+      GlobalKey<NavigatorState>();
 
   static CustomRouter get instance => _instance;
 
@@ -20,55 +34,102 @@ class CustomRouter {
 
   CustomRouter._internal() {
     final routes = [
-        GoRoute(
-          path: splashScreenRoute,
-          pageBuilder: (BuildContext context, GoRouterState state) {
-            return getPage(
-                child: const SplashScreen(),
-                state: state,
-            );
-          },
-        ),
+      StatefulShellRoute.indexedStack(
+        parentNavigatorKey: parentNavigatorKey,
+        branches: [
+          StatefulShellBranch(
+            navigatorKey: homeTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: homePath,
+                pageBuilder: (context, GoRouterState state) {
+                  return getPage(
+                    child: const HomePage(),
+                    state: state,
+                  );
+                },
+              ),
+              GoRoute(
+                path: eventDetailRoute,
+                pageBuilder: (context, state) {
+                  final id = int.tryParse(state.pathParameters['id'] ?? '');
 
-        GoRoute(
-          path: mainScreenRoute,
-          pageBuilder: (BuildContext context, GoRouterState state) {
-            return getPage(
-                child: const MainScreen(),
-                state: state,
-            );
-          },
-        ),
+                  final extra = state.extra as Map<String, dynamic>;
+                  EventModel eventModel = extra['eventModel'] as EventModel;
+                  Function(int) likeEvent = extra['likeEvent'] as Function(int);
 
-        GoRoute(
-            path: loginRoute,
-            pageBuilder: (BuildContext context, GoRouterState state) {
-              return getPage(
-                  child: const LoginPage(),
-                  state: state,
-              );
-            },
-        ),
-
-        GoRoute(
-            path: registerRoute,
-            pageBuilder: (BuildContext context, GoRouterState state) {
-              return getPage(
-                  child: const RegisterPage(),
-                  state: state,
-              );
-            },
-        ),
-
-        GoRoute(
-            path: validationRoute,
-            pageBuilder: (BuildContext context, GoRouterState state) {
-              return getPage(
-                  child: const ValidationPage(),
-                  state: state,
-              );
-            },
-        ),
+                  return getPage(
+                    child: EventDetailPage(eventModel: eventModel, likeEvent: likeEvent),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+          StatefulShellBranch(
+            navigatorKey: settingsTabNavigatorKey,
+            routes: [
+              GoRoute(
+                path: settingsRoute,
+                pageBuilder: (context, state) {
+                  return getPage(
+                    child: const SettingsPage(),
+                    state: state,
+                  );
+                },
+              ),
+            ],
+          ),
+        ],
+        pageBuilder: (
+          BuildContext context,
+          GoRouterState state,
+          StatefulNavigationShell navigationShell,
+        ) {
+          return getPage(
+            child: BottomNavigationPage(
+              child: navigationShell,
+            ),
+            state: state,
+          );
+        },
+      ),
+      GoRoute(
+        path: splashScreenRoute,
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return getPage(
+            child: const SplashScreen(),
+            state: state,
+          );
+        },
+      ),
+      GoRoute(
+        path: loginRoute,
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return getPage(
+            child: const LoginPage(),
+            state: state,
+          );
+        },
+      ),
+      GoRoute(
+        path: registerRoute,
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return getPage(
+            child: const RegisterPage(),
+            state: state,
+          );
+        },
+      ),
+      GoRoute(
+        path: validationRoute,
+        pageBuilder: (BuildContext context, GoRouterState state) {
+          return getPage(
+            child: const ValidationPage(),
+            state: state,
+          );
+        },
+      ),
     ];
 
     router = GoRouter(
--- lib/router/route_constants.dart
+++ lib/router/route_constants.dart
@@ -1,5 +1,10 @@
 const String splashScreenRoute = "/";
 const String loginRoute = "/login";
 const String registerRoute = "/register";
-const String mainScreenRoute = "/mainScreen";
 const String validationRoute = "/validation";
+
+const String homePath = "/home";
+const String settingsRoute = "/settings";
+const String profileRoute = "/profile";
+
+const String eventDetailRoute = "/eventDetail/:id";
