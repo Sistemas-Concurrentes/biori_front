fad59e29fb5f406a782595772aec5d4df10f27a3
JaviGP2002 <javigp2002@gmail.com>
2024-04-13 20:07:38 +0200

    :rocket: Releases datasource from local or api, #8,
    Realmente ldesde la api se están enviando todos los vlaores a la vez, se mantendrán ambas opciones por si se cambia a los repositorios en un futuro de cada tipo pero ahora se recibe todo desde el de releases.
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

---
+++ lib/main_screen/home/user_stories/releases/datasource/api_releases_datasource.dart
@@ -0,0 +1,20 @@
+import 'dart:convert';
+import 'package:biori/conection_to_backend/requests.dart';
+import 'package:biori/style/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'releases_datasource.dart';
+
+class ApiReleasesDatasource implements ReleasesDatasource {
+  final ApiService apiService = ApiService();
+  static String uri = Constants.URI;
+  static String getAllUri = '$uri/releases/getAll';
+
+  @override
+  Future<Map<String, dynamic>> getReleases() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final String? token = prefs.getString(Constants.TOKEN);
+
+    final object = await apiService.getRequestWithHeader(getAllUri, token);
+    return jsonDecode(object.body) as Map<String, dynamic>;
+  }
+}
---
+++ lib/main_screen/home/user_stories/releases/datasource/local_releases_datasource.dart
@@ -0,0 +1,9 @@
+import 'releases_datasource.dart';
+
+class LocalReleasesDatasource implements ReleasesDatasource {
+  @override
+  Future<Map<String, dynamic>> getReleases() {
+
+    throw UnimplementedError();
+  }
+}
