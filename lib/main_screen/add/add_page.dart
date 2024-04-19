262ada2551187b6e0763752620da806b65d6aeb5
JaviGP2002 <javigp2002@gmail.com>
2024-04-19 21:23:45 +0200

    :zap: Add: and tiles tags subroutes and send them to the pages
    LAs tags sierven para una animación más dinámica.
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/add_page.dart
+++ lib/main_screen/add/add_page.dart
@@ -1,3 +1,6 @@
+import 'package:biori/main_screen/add/constants/add_constants.dart';
+import 'package:biori/router/custom_router.dart';
+import 'package:biori/router/route_constants.dart';
 import 'package:biori/style/javi_edit_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -25,9 +28,39 @@ class _AddPageState extends State<AddPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              createCardToAdd(context, "$anadir $evento", Icons.event, () {}),
-              createCardToAdd(context, "$anadir $advertisement", Icons.note_add, () {}),
-              createCardToAdd(context, "$anadir $report", Icons.folder_open, () {}),
+              createCardToAdd(
+                context,
+                AddConstants().eventTag,
+                "$anadir $evento",
+                Icons.event,
+                () {
+                  CustomRouter.router
+                      .push("$addReleasesRoute/$addEventRoute")
+                      .then((value) => null);
+                },
+              ),
+              createCardToAdd(
+                context,
+                AddConstants().advertisementTag,
+                "$anadir $advertisement",
+                Icons.note_add,
+                () {
+                  CustomRouter.router
+                      .push("$addReleasesRoute/$addAdvertisementRoute")
+                      .then((value) => null);
+                },
+              ),
+              createCardToAdd(
+                context,
+                AddConstants().reportTag,
+                "$anadir $report",
+                Icons.folder_open,
+                () {
+                  CustomRouter.router
+                      .push("$addReleasesRoute/$addReportRoute")
+                      .then((value) => null);
+                },
+              ),
             ],
           ),
         ),
@@ -35,14 +68,15 @@ class _AddPageState extends State<AddPage> {
     );
   }
 
-  Widget createCardToAdd(context, text, icon, Function pushNewPageFunction) {
+  Widget createCardToAdd(
+      context, tagHero, text, icon, Function pushNewPageFunction) {
     return Hero(
-      tag: "name-tile",
+      tag: tagHero,
       child: Card(
           child: ListTile(
         title: Text(text, style: JaviStyle.tituloEvento),
         trailing: Icon(icon),
-        onTap: () => pushNewPageFunction,
+        onTap: () => pushNewPageFunction(),
       )),
     );
   }
