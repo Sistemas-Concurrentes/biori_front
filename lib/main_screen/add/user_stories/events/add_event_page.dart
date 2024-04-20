e7e9669c71810f5179b6cc85902d4f020faae04c
JaviGP2002 <javigp2002@gmail.com>
2024-04-20 20:58:26 +0200

    :globe_with_meridians: :sparkles: Add title field on add event page
    Ponemos el icono de Title, y que salga el hint text según la localización a través del field widget
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/user_stories/events/add_event_page.dart
+++ lib/main_screen/add/user_stories/events/add_event_page.dart
@@ -1,5 +1,9 @@
 import 'package:biori/main_screen/add/constants/add_constants.dart';
+import 'package:biori/style/javi_edit_text.dart';
+import 'package:biori/style/widgets_javi.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 
 class AddEventPage extends StatefulWidget {
   const AddEventPage({super.key});
@@ -9,9 +13,22 @@ class AddEventPage extends StatefulWidget {
 
 class _AddEventPageState extends State<AddEventPage> {
   final _formKey = GlobalKey<FormState>();
+  final widgetsJavi = WidgetsJavi();
+
+  String? titulo;
+  String? descripcion;
+  String? categoria;
+  String? localizacion;
+  List<DateTime> datesFromEvent = [];
+  List<TagsButtonsModel> tags = [];
 
   @override
   Widget build(BuildContext context) {
+    var formWidgets = [
+      eventTitleEditText(_onValidateTitle),
+
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Hero(
@@ -19,14 +36,34 @@ class _AddEventPageState extends State<AddEventPage> {
           child: const Text('Add Event'),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: const Column(
-          children: <Widget>[
-            // Add TextFormFields and ElevatedButton here.
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(JaviPaddings.L),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: formWidgets.map((widget) => widgetsJavi.paddedWidget(widget)).toList(),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  eventTitleEditText(Function onValidate) {
+    return JaviForms.inputFieldWidget(
+      context,
+      "eventTitle",
+      "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.evento}",
+      onValidate,
+          (onSavedVal) => { titulo = onSavedVal},
+      prefixIcon: const Icon(Icons.title),
+
+    );
+  }
+  _onValidateTitle(String onValidateVal){
+
   }
 }
