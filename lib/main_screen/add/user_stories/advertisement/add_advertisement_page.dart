af473ead06438e19147ba5b817311a0a2c66a6cb
JaviGP2002 <javigp2002@gmail.com>
2024-04-24 18:13:41 +0200

    :globe_with_meridians: :rocket: Introduce add Advertisement for all groups
    Los grupos que se pueden seleccionar son aquellos que el profesor tiene derecho a mandar advertisements.
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/user_stories/advertisement/add_advertisement_page.dart
+++ lib/main_screen/add/user_stories/advertisement/add_advertisement_page.dart
@@ -1,32 +1,130 @@
 import 'package:biori/main_screen/add/constants/add_constants.dart';
+import 'package:biori/main_screen/home/user_stories/releases/releases_widgets/button_widgets/model/categories_button_model.dart';
+import 'package:biori/style/javi_edit_text.dart';
+import 'package:biori/style/model/chip_button_model.dart';
+import 'package:biori/style/widgets_javi.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AddAdvertisementPage extends StatefulWidget {
   const AddAdvertisementPage({super.key});
 
   @override
-  State<AddAdvertisementPage> createState() => _AddAdvertisementPageState();}
+  State<AddAdvertisementPage> createState() => _AddAdvertisementPageState();
+}
 
 class _AddAdvertisementPageState extends State<AddAdvertisementPage> {
   final _formKey = GlobalKey<FormState>();
+  final widgetsJavi = WidgetsJavi();
+
+  String? titulo;
+  String? descripcion;
+  List<ChipButtonModel> tagsButtons = [];
+  List<ChipButtonModel> allTagsButtons = [];
+
+  @override
+  void initState() {
+    super.initState();
+    allTagsButtons.addAll([
+      ChipButtonModel(name: "Grupo Organica A1", id: 1),
+      ChipButtonModel(name: "Grupo Organica A2", id: 2),
+      ChipButtonModel(name: "Grupo Organica A3", id: 3),
+      ChipButtonModel(name: "SubGrupos Organica A", id: 4),
+    ]);
+  }
 
   @override
   Widget build(BuildContext context) {
+    var formWidgets = [
+      eventTitleEditText(_onValidateTitle),
+      descriptionBigEditText(_onValidateDescription),
+      tagsCheckBoxes(_onValidateTags),
+      submitButton(context),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Hero(
-          tag: AddConstants().advertisementTag,
-          child: const Text('Add Advertisement'),
+          tag: AddConstants().eventTag,
+          child: Text(
+              "${AppLocalizations.of(context)!.anadir} ${AppLocalizations.of(context)!.advertisement}"),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: const Column(
-          children: <Widget>[
-            // Add TextFormFields and ElevatedButton here.
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(JaviPaddings.L),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: formWidgets
+                  .map((widget) => widgetsJavi.paddedWidget(widget))
+                  .toList(),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  eventTitleEditText(Function onValidate) {
+    return JaviForms.inputFieldWidget(
+      context,
+      "eventTitle",
+      "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.advertisement}",
+      onValidate,
+      (onSavedVal) => {titulo = onSavedVal},
+      prefixIcon: const Icon(Icons.title),
+    );
+  }
+
+  descriptionBigEditText(Function onValidate) {
+    return JaviForms.inputFieldWidget(
+        context,
+        "eventDescription",
+        AppLocalizations.of(context)!.descripcion,
+        onValidate,
+        (onSavedVal) => {descripcion = onSavedVal},
+        prefixIcon: const Icon(Icons.description),
+        maxLines: null);
+  }
+
+  tagsCheckBoxes(Function onValidate) {
+    return JaviForms.chipsInputFieldWidget(context, allTagsButtons, onValidate,
+        (onSavedVal) => {tagsButtons = onSavedVal},
+        titleEvent: AppLocalizations.of(context)!.seleccionaGrupos);
+  }
+
+  submitButton(BuildContext context) {
+    return JaviForms.submitButton(context, AppLocalizations.of(context)!.send,
+        () {
+      if (_formKey.currentState!.validate()) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Evento creado correctamente!")));
+        _formKey.currentState!.save();
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Evento creado mal!")));
+      }
+    });
+  }
+
+  _onValidateTitle(String onValidateVal) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
+  }
+
+  _onValidateDescription(String onValidateVal) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.descripcion} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
+  }
+
+  String? _onValidateTags(List<TagsButtonsModel>? onValidateVal) {
+    return (onValidateVal == null || onValidateVal.isEmpty)
+        ? AppLocalizations.of(context)!.mustSelectOneOrMore
+        : null;
   }
 }
