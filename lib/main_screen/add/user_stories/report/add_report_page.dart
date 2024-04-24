52f493239314bc24511c9018927770c18436d9b8
JaviGP2002 <javigp2002@gmail.com>
2024-04-24 18:17:53 +0200

    :rocket: Add reportPage to add new reports
    Los nuevos reports que van dirigidos a todos los usuarios solo puede acceder a ellos el/los coordinadores
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/user_stories/report/add_report_page.dart
+++ lib/main_screen/add/user_stories/report/add_report_page.dart
@@ -1,33 +1,104 @@
+import 'package:biori/main_screen/add/constants/add_constants.dart';
+import 'package:biori/main_screen/home/user_stories/releases/releases_widgets/button_widgets/model/categories_button_model.dart';
+import 'package:biori/style/javi_edit_text.dart';
+import 'package:biori/style/model/chip_button_model.dart';
+import 'package:biori/style/widgets_javi.dart';
 import 'package:flutter/material.dart';
-
-import '../../constants/add_constants.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class AddReportPage extends StatefulWidget {
   const AddReportPage({super.key});
 
   @override
-  State<AddReportPage> createState() => _AddReportPageState();}
+  State<AddReportPage> createState() => _AddReportPageState();
+}
 
 class _AddReportPageState extends State<AddReportPage> {
   final _formKey = GlobalKey<FormState>();
+  final widgetsJavi = WidgetsJavi();
+
+  String? titulo;
+  String? descripcion;
 
   @override
   Widget build(BuildContext context) {
+    var formWidgets = [
+      eventTitleEditText(_onValidateTitle),
+      descriptionBigEditText(_onValidateDescription),
+      submitButton(context),
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: Hero(
-          tag: AddConstants().reportTag,
-          child: const Text('Add Report'),
+          tag: AddConstants().eventTag,
+          child: Text(
+              "${AppLocalizations.of(context)!.anadir} ${AppLocalizations.of(context)!.report}"),
         ),
       ),
-      body: Form(
-        key: _formKey,
-        child: const Column(
-          children: <Widget>[
-            // Add TextFormFields and ElevatedButton here.
-          ],
+      body: Padding(
+        padding: const EdgeInsets.all(JaviPaddings.L),
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: formWidgets
+                  .map((widget) => widgetsJavi.paddedWidget(widget))
+                  .toList(),
+            ),
+          ),
         ),
       ),
     );
+  }
+
+  eventTitleEditText(Function onValidate) {
+    return JaviForms.inputFieldWidget(
+      context,
+      "eventTitle",
+      "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.report}",
+      onValidate,
+      (onSavedVal) => {titulo = onSavedVal},
+      prefixIcon: const Icon(Icons.title),
+    );
+  }
+
+  descriptionBigEditText(Function onValidate) {
+    return JaviForms.inputFieldWidget(
+        context,
+        "eventDescription",
+        AppLocalizations.of(context)!.descripcion,
+        onValidate,
+        (onSavedVal) => {descripcion = onSavedVal},
+        prefixIcon: const Icon(Icons.description),
+        maxLines: null);
+  }
+
+  submitButton(BuildContext context) {
+    return JaviForms.submitButton(context, AppLocalizations.of(context)!.send,
+        () {
+      if (_formKey.currentState!.validate()) {
+        ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text("Evento creado correctamente!")));
+        _formKey.currentState!.save();
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text("Evento creado mal!")));
+      }
+    });
+  }
+
+  _onValidateTitle(String onValidateVal) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
+  }
+
+  _onValidateDescription(String onValidateVal) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.descripcion} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
   }
 }
