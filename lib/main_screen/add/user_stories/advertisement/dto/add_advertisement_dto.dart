284479c9b74451b3dfeb9fde706b07d2a6a0e62f
JaviGP2002 <javigp2002@gmail.com>
2024-04-28 19:08:35 +0200

    :sparkles: Add AddAdvertisementDto with the information needed for an adv on its creation
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

---
+++ lib/main_screen/add/user_stories/advertisement/dto/add_advertisement_dto.dart
@@ -0,0 +1,19 @@
+import 'package:biori/style/model/chip_button_model.dart';
+
+class AddAdvertisementDto {
+  String? titulo;
+  String? descripcion;
+  List<ChipButtonModel> tagsButtons = [];
+
+  AddAdvertisementDto({
+    required this.titulo,
+    required this.descripcion,
+    required this.tagsButtons,
+  });
+
+  Map<String, dynamic> toJson() => {
+        'titulo': titulo,
+        'descripcion': descripcion,
+        'tagsButtons': tagsButtons.map((e) => e.toJson()).toList(),
+      };
+}
