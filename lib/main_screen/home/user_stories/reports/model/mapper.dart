cec0208331d51d2ee6f4092f5808c7335508bd57
JaviGP2002 <javigp2002@gmail.com>
2024-04-29 19:38:03 +0200

    :rocket: Improve performance of report mapper and dto with factory
    los factory se utilizan para crear a través de json y así, además, en el mapper podemos crear un primer barrido y comprobar que todos los report Model creados tienen toda la información
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/home/user_stories/reports/model/mapper.dart
+++ lib/main_screen/home/user_stories/reports/model/mapper.dart
@@ -3,14 +3,7 @@ import 'package:biori/main_screen/home/user_stories/reports/model/report_model.d
 class ReportMapper {
   List<ReportModel> fromJson(List<dynamic> listAdvertisementsJson) {
     return listAdvertisementsJson
-        .map((advertisementJson) => ReportModel(
-              id: advertisementJson['id'],
-              title: advertisementJson['title'],
-              description: advertisementJson['description'],
-              creatorName: advertisementJson['teacherName'],
-              lastUpdate: DateTime.parse(advertisementJson['updatedAt']),
-              teacherUserId: advertisementJson['teacherId'],
-            ))
+        .map((reportJson) => ReportModel.fromJson(reportJson))
         .toList();
   }
 }
