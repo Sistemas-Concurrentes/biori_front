ee9b49cafe89a6b17089e9e6f44f31bc3ffcf4c6
JaviGP2002 <javigp2002@gmail.com>
2024-05-01 19:22:19 +0200

    :fire: DELETE: ALL unusued methods and files
    Estos archivos se crearon con otro proposito y no tienen que seguir puesto que ya no son necesarios en ningun modo
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/home/user_stories/events/datasource/event_datasource.dart
+++
@@ -1,7 +0,0 @@
-import 'package:biori/main_screen/home/user_stories/events/model/event_model.dart';
-
-abstract class EventsDatasource {
-  Future<List<EventModel>> getEventsOrderedByLastUpdate();
-  Future<List<int>> getCategoriesFollowedByUser();
-}
-
