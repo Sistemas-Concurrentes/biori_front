23d761d2f17d0621478e775c87f7a7e76015ed62
JaviGP2002 <javigp2002@gmail.com>
2024-05-01 20:23:22 +0200

    :boom: Add Report Output depending on http.response.status
    Hay que avisar al usuario que está pasando siempre
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/user_stories/report/user_stories/add_report.dart
+++ lib/main_screen/add/user_stories/report/user_stories/add_report.dart
@@ -2,11 +2,17 @@ import 'package:biori/conection_to_backend/requests.dart';
 import 'package:biori/main_screen/add/user_stories/report/dto/add_report_dto.dart';
 import 'package:biori/style/constants.dart';
 
+enum AddReportOutput {
+  created,
+  error,
+  forbidden,
+}
+
 class AddReport {
   static String uri = Constants.URI;
   static String addEventUri = '$uri/releases/addReport';
 
-  Future<dynamic> run(
+  Future<AddReportOutput> run(
     String titulo,
     String descripcion,
   ) async {
@@ -15,6 +21,14 @@ class AddReport {
       descripcion: descripcion,
     ).toJson();
 
-    return await ApiService().postRequestWithHeader(addEventUri, data);
+    var response = await ApiService().postRequestWithHeader(addEventUri, data);
+
+    if (response.statusCode == 201) {
+      return AddReportOutput.created;
+    } else if (response.statusCode == 403) {
+      return AddReportOutput.forbidden;
+    } else {
+      return AddReportOutput.error;
+    }
   }
 }
