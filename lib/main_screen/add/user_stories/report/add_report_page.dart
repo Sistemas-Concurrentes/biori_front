6a00ce919419cc18972c5efd664fae61a01c193e
JaviGP2002 <javigp2002@gmail.com>
2024-06-17 19:00:59 +0200

    :building_construction: Introduce viewModel to ReportPage
    El viewModel será el encargado de manejar todas las variables y funciones
    
    Signed-off-by: JaviGP2002 <javigp2002@gmail.com>

--- lib/main_screen/add/user_stories/report/add_report_page.dart
+++ lib/main_screen/add/user_stories/report/add_report_page.dart
@@ -1,12 +1,13 @@
+import 'dart:async';
+
 import 'package:biori/main_screen/add/constants/add_constants.dart';
-import 'package:biori/main_screen/add/user_stories/report/repository/add_report_repository.dart';
-import 'package:biori/main_screen/add/user_stories/report/user_stories/add_report.dart';
-import 'package:biori/router/custom_router.dart';
 import 'package:biori/style/javi_edit_text.dart';
 import 'package:biori/style/model/chip_button_model.dart';
 import 'package:biori/style/widgets_javi.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import 'add_report_page_viewmodel.dart';
 
 class AddReportPage extends StatefulWidget {
   const AddReportPage({super.key});
@@ -16,22 +17,28 @@ class AddReportPage extends StatefulWidget {
 }
 
 class _AddReportPageState extends State<AddReportPage> {
-  final _formKey = GlobalKey<FormState>();
   final widgetsJavi = WidgetsJavi();
+  final List<StreamSubscription> subscriptions = [];
+  final viewModel = AddReportPageVM();
 
-  String titulo = "";
-  String descripcion = "";
   List<ChipButtonModel> courses = [];
-  bool toTeachers = false;
 
   bool _isApiCallProcess = false;
 
   @override
+  void initState() {
+    super.initState();
+    _subscribeAllToViewModel();
+    viewModel.loadCourses();
+  }
+
+  @override
   Widget build(BuildContext context) {
     var formWidgets = [
-      eventTitleEditText(_onValidateTitle),
-      descriptionBigEditText(_onValidateDescription),
-      tagsCheckBoxes(_onValidateTags),
+      eventTitleEditText(viewModel.onValidateTitle, viewModel.onSavedValTitle),
+      descriptionBigEditText(
+          viewModel.onValidateDescription, viewModel.onSavedValDescription),
+      tagsCheckBoxes(viewModel.onValidateTags, viewModel.onSavedValTags),
       submitButton(context),
     ];
 
@@ -49,7 +56,7 @@ class _AddReportPageState extends State<AddReportPage> {
           context,
           _isApiCallProcess,
           Form(
-            key: _formKey,
+            key: viewModel.formKey,
             child: SingleChildScrollView(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -65,124 +72,73 @@ class _AddReportPageState extends State<AddReportPage> {
     );
   }
 
-  eventTitleEditText(Function onValidate) {
+  eventTitleEditText(Function onValidate, Function onSaved) {
     return JaviForms.inputFieldWidget(
       context,
       "eventTitle",
       "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.report}",
-      onValidate,
-      (onSavedVal) => {titulo = onSavedVal},
+      (onValidateVal) => onValidate(onValidateVal, context),
+      (onSavedVal) => onSaved(onSavedVal),
       prefixIcon: const Icon(Icons.title),
     );
   }
 
-  descriptionBigEditText(Function onValidate) {
+  descriptionBigEditText(Function onValidate, Function onSaved) {
     return JaviForms.inputFieldWidget(
         context,
         "eventDescription",
         AppLocalizations.of(context)!.descripcion,
-        onValidate,
-        (onSavedVal) => {descripcion = onSavedVal},
+        (onValidateVal) => onValidate(onValidateVal, context),
+        (onSavedVal) => onSaved(onSavedVal),
         prefixIcon: const Icon(Icons.description),
         maxLines: null);
   }
 
-  Widget tagsCheckBoxes(Function onValidate) {
-    return FutureBuilder<List<ChipButtonModel>>(
-      future: AddReportRepository().getAllCourses(),
-      builder: (BuildContext context,
-          AsyncSnapshot<List<ChipButtonModel>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const CircularProgressIndicator();
-        } else if (snapshot.hasError) {
-          return const Text('Error al cargar los datos');
-        } else {
-          return JaviForms.chipsInputFieldWidget(
-              context,
-              snapshot.data!,
-              onValidate,
-              (onSavedVal) => {
-                    courses = onSavedVal,
-                    toTeachers = courses.any((element) => element.id == 0),
-                    if (toTeachers)
-                      courses.removeWhere((element) => element.id == 0)
-                  },
-              titleEvent: AppLocalizations.of(context)?.seleccionaGrupos ?? "");
-        }
-      },
-    );
+  Widget tagsCheckBoxes(Function onValidate, Function onSaved) {
+    if (courses.isEmpty) {
+      return const CircularProgressIndicator();
+    } else {
+      return JaviForms.chipsInputFieldWidget(
+          context,
+          courses,
+          (onValidateVal) => onValidate(onValidateVal, context),
+          (onSavedVal) => onSaved(onSavedVal),
+          titleEvent: AppLocalizations.of(context)?.seleccionaGrupos ?? "");
+    }
   }
 
   submitButton(BuildContext context) {
     return JaviForms.submitButton(context, AppLocalizations.of(context)!.send,
-        () async {
-      if (!_formKey.currentState!.validate()) {
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text(AppLocalizations.of(context)!.errorForm)));
-        return;
-      }
-      _formKey.currentState!.save();
-
-      _showLoading(true);
-      var addReportOutput =
-          await AddReport().run(titulo, descripcion, courses, toTeachers);
-      _showLoading(false);
-      _showDialogAfterApiCall(addReportOutput);
+        () {
+      viewModel.submitButton(context);
     });
   }
 
-  _onValidateTitle(String onValidateVal) {
-    return onValidateVal.isEmpty
-        ? "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.cannotBeEmpty}"
-        : null;
-  }
-
-  _onValidateDescription(String onValidateVal) {
-    return onValidateVal.isEmpty
-        ? "${AppLocalizations.of(context)!.descripcion} ${AppLocalizations.of(context)!.cannotBeEmpty}"
-        : null;
-  }
-
-  String? _onValidateTags(List<ChipButtonModel>? onValidateVal) {
-    return (onValidateVal == null || onValidateVal.isEmpty)
-        ? AppLocalizations.of(context)!.mustSelectOneOrMore
-        : null;
-  }
-
-  _showLoading(bool showLoading) {
-    setState(() {
-      _isApiCallProcess = showLoading;
-    });
-  }
-
-  _showDialogAfterApiCall(AddReportOutput addReportOutput) {
-    String titleDialog = "";
-    Icon? iconDialog;
-    Function onPressed = () {
-      CustomRouter.router.pop();
-    };
-
-    if (addReportOutput == AddReportOutput.created) {
-      titleDialog = context.mounted
-          ? AppLocalizations.of(context)?.reportCreado ?? ""
-          : "";
-      iconDialog = const Icon(Icons.check);
-    } else if (addReportOutput == AddReportOutput.forbidden) {
-      titleDialog = context.mounted
-          ? AppLocalizations.of(context)?.errorPermisos ?? ""
-          : "";
-      iconDialog = const Icon(Icons.sms_failed);
-    } else {
-      titleDialog = context.mounted
-          ? AppLocalizations.of(context)?.errorCrearReport ?? ""
-          : "";
-      iconDialog = const Icon(Icons.error);
-      onPressed = () {};
+  @override
+  void dispose() {
+    for (var element in subscriptions) {
+      element.cancel();
     }
+    super.dispose();
+  }
 
-    if (context.mounted) {
-      widgetsJavi.showDialogWithText(context, titleDialog, onPressed,
-          icon: iconDialog);
-    }
+  void _subscribeAllToViewModel() {
+    subscriptions.add(viewModel.courses.stream.listen((courses) {
+      setState(() {
+        this.courses = courses;
+      });
+    }));
+
+    subscriptions.add(viewModel.isApiCallProcess.stream.listen((courses) {
+      setState(() {
+        _isApiCallProcess = false;
+      });
+    }));
+
+    subscriptions.add(viewModel.responseDialog.listen((responseDialog) {
+      widgetsJavi.showDialogWithText(
+          context, responseDialog.title, responseDialog.onPressed,
+          icon: responseDialog.icon);
+    }));
   }
 }
---
+++ lib/main_screen/add/user_stories/report/add_report_page_viewmodel.dart
@@ -0,0 +1,122 @@
+import 'package:biori/main_screen/add/user_stories/report/repository/add_report_repository.dart';
+import 'package:biori/main_screen/add/user_stories/report/user_stories/add_report.dart';
+import 'package:biori/router/custom_router.dart';
+import 'package:biori/style/model/chip_button_model.dart';
+import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class AddReportPageVM {
+  final reportRepository = AddReportRepository();
+  BehaviorSubject<List<ChipButtonModel>> courses = BehaviorSubject.seeded([]);
+  BehaviorSubject<bool> isApiCallProcess = BehaviorSubject.seeded(false);
+  BehaviorSubject<ResponseDialog> responseDialog = BehaviorSubject();
+
+  final formKey = GlobalKey<FormState>();
+  String titulo = "";
+  String descripcion = "";
+  List<ChipButtonModel> coursesToSend = [];
+  bool toTeachers = false;
+
+  void loadCourses() async {
+    final currentCourses = courses.valueOrNull ?? [];
+
+    if (currentCourses.isNotEmpty) {
+      courses.add(currentCourses);
+      return;
+    }
+
+    final newCourses = await reportRepository.getAllCourses();
+    courses.add(newCourses);
+  }
+
+  onValidateTitle(String onValidateVal, context) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.titulo} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
+  }
+
+  onValidateDescription(String onValidateVal, context) {
+    return onValidateVal.isEmpty
+        ? "${AppLocalizations.of(context)!.descripcion} ${AppLocalizations.of(context)!.cannotBeEmpty}"
+        : null;
+  }
+
+  String? onValidateTags(List<ChipButtonModel>? onValidateVal, context) {
+    return (onValidateVal == null || onValidateVal.isEmpty)
+        ? AppLocalizations.of(context)!.mustSelectOneOrMore
+        : null;
+  }
+
+  onSavedValTags(List<ChipButtonModel> onSavedVal) {
+    coursesToSend = onSavedVal;
+    toTeachers = coursesToSend.any((element) => element == 0);
+    if (toTeachers) {
+      coursesToSend.removeWhere((course) => course == 0);
+    }
+  }
+
+  onSavedValTitle(String onSavedVal) {
+    titulo = onSavedVal;
+  }
+
+  onSavedValDescription(String onSavedVal) {
+    descripcion = onSavedVal;
+  }
+
+  void submitButton(BuildContext context) async {
+    if (!formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(AppLocalizations.of(context)!.errorForm)));
+      return;
+    }
+    formKey.currentState!.save();
+
+    isApiCallProcess.add(true);
+    var addReportOutput =
+        await AddReport().run(titulo, descripcion, coursesToSend, toTeachers);
+
+    isApiCallProcess.add(false);
+
+    if (context.mounted) {
+      responseDialog.add(responseDialogFromOutput(context, addReportOutput));
+    }
+  }
+
+  ResponseDialog responseDialogFromOutput(
+      BuildContext context, AddReportOutput addReportOutput) {
+    String titleDialog;
+    Icon iconDialog;
+    Function onPressed = () {
+      CustomRouter.router.pop();
+    };
+
+    if (addReportOutput == AddReportOutput.created) {
+      titleDialog = context.mounted
+          ? AppLocalizations.of(context)?.reportCreado ?? ""
+          : "";
+      iconDialog = const Icon(Icons.check);
+    } else if (addReportOutput == AddReportOutput.forbidden) {
+      titleDialog = context.mounted
+          ? AppLocalizations.of(context)?.errorPermisos ?? ""
+          : "";
+      iconDialog = const Icon(Icons.sms_failed);
+    } else {
+      titleDialog = context.mounted
+          ? AppLocalizations.of(context)?.errorCrearReport ?? ""
+          : "";
+      iconDialog = const Icon(Icons.error);
+      onPressed = () {};
+    }
+
+    return ResponseDialog(titleDialog, iconDialog, onPressed);
+  }
+}
+
+class ResponseDialog {
+  final String title;
+  final Icon icon;
+  final Function onPressed;
+
+  ResponseDialog(this.title, this.icon, this.onPressed);
+}
